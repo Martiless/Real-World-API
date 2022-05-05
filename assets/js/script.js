9b9fa5a29f4ab2b01e19f3aeca85da862408a5ef
Martina Martin <marti.law@gmail.com>
2022-05-05 10:33:51 +0000

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const API_KEY = "oECn-SMy4AQs6gqO5amvEAbrF-0";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
+
+document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById('submit').addEventListener('click', e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById('checksform'));
+    for (let e of form.entries()) {
+        console.log(e);
+    }
+
+}
+
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if(response.ok){
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+
+    }
+}
+
+
+function displayStatus(data) {
+    let heading = "API KEY STATUS";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+      
+    document.getElementById('resultsModalTitle').innerText = heading;
+    document.getElementById('results-content').innerHTML = results;
+
+    resultsModal.show();
+}
